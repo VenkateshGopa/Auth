6cdaa3e2c03454e4efc92ac8ea6d39f62a249cf9
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-11-12 22:28:43 +0530

    Update test.js

--- Routes/test.js
+++ Routes/test.js
@@ -2,9 +2,19 @@ const express = require('express');
 const mongo = require('../db/mongo');
 const router= express.Router();
 const{ObjectId} = require('mongodb')
-router.get('/data', async (req, res)=>{
+router.get('/note', async (req, res)=>{
     const data =await mongo.db.collection('userdetails').findOne({_id:ObjectId(req.user)})
-    res.send(data);
+    const data1 =await mongo.db.collection('notes').findOne({email:data.email})
+    res.send({firstname:data.firstname, lastname:data.lastname, note:data1.note, email:data.email });
 })
 
+router.post('/note' ,async (req, res)=>{
+    try{
+    await mongo.db.collection('notes').updateOne({email:req.body.email},{$set:{note: req.body.note}})
+    res.status(200).send("saved");
+    }
+    catch{
+        res.status(400).send({error:"failed to save data"})
+    }
+})
 module.exports=router;
