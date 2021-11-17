ccf0ef07e5d47db2ba3f0465120b61eda6ae2f1f
VenkateshGopa <85928554+VenkateshGopa@users.noreply.github.com>
2021-11-17 21:53:46 +0530

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -9,6 +9,7 @@ Router.post('/login', services.login);
 Router.post('/forgotPassword', services.forgotpassword);
 Router.post('/verification' ,services.checkcode);
 Router.post('/Newpassword', services.createnewpassword);
+Router.post('/linkvalid' , services.linkvalid)
 
 
 module.exports = Router;
